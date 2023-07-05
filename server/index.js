9016855b74e1b8a98aa7b9ef771eb863fabb3fc4
Felix Stiffel <felix.stiffel@hotmail.de>
2023-07-05 14:20:17 +0200

    comment gay shit

--- server/index.js
+++ server/index.js
@@ -15,15 +15,15 @@ app.use(function timeLog(req, res, next) {
 
 app.use(express.static("public"));
 
-app.use("/api/scoreboard", require("./routes/scoreboard"));
+//app.use("/api/scoreboard", require("./routes/scoreboard"));
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
 //cron.schedule("* * * * *", () => {
 // For quicker testing
-cron.schedule("0 4 * * *", () => {
+/*cron.schedule("0 4 * * *", () => {
   // 4 uhr nachts
   console.log(
     `Fetching new scoreboard data at ${new Date().toLocaleTimeString()}`
   );
   CONTRACT.fetchHoldersFromContract();
-});
+});*/
